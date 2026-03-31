7df0a7b0e18d2966335c691312a823d3a818b2d8
PILN <matveys909@gmail.com>
2026-03-31 17:10:35 +0300

    Initial Commit

---
+++ check_ocr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  check_ocr
+//
+//  Created by Матвей Сорокин on 31.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ check_ocr/check_ocrApp.swift
@@ -0,0 +1,17 @@
+//
+//  check_ocrApp.swift
+//  check_ocr
+//
+//  Created by Матвей Сорокин on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct check_ocrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
