0da760fb2bba31946c592d1336a63a3904849796
PILN <matveys909@gmail.com>
2026-04-07 15:41:19 +0300

    fix: conflicts

--- SplitApp/Features/Events/ViewModels/EventsHomeViewModel.swift
+++ SplitApp/Features/Events/ViewModels/EventsHomeViewModel.swift
@@ -53,9 +53,7 @@ final class EventsHomeViewModel: ObservableObject {
 }
 
 extension EventsHomeViewModel {
-    @MainActor static func mock(
-        service: EventManagementServiceProtocol = EventManagementService()
-    ) -> EventsHomeViewModel {
+    @MainActor static func mock(service: EventManagementServiceProtocol = EventManagementService()) -> EventsHomeViewModel {
         let viewModel = EventsHomeViewModel(service: service)
         Task { await viewModel.loadDataIfNeeded() }
         return viewModel
